1de3442b8c74df1d18d034fb9e6473ef2cd3e39d
JOJO <1654441515@qq.com>
2022-07-13 11:50:20 +0800

    '初始化项目'

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '未来首富',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主体内容是否固定
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
