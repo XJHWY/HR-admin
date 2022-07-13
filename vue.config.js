0cec012b1d0dc5fb50f3ab62fec1b3085435b9d4
JOJO <1654441515@qq.com>
2022-07-13 17:24:10 +0800

    '提交修改'

--- vue.config.js
+++ vue.config.js
@@ -35,8 +35,18 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    }
+    },
     // before: require('./mock/mock-server.js')
+    // 反向代理
+    proxy: {
+      '/abc': {
+        target: 'http://ihrm.itheima.net/',
+        changeOrigin: true,
+        pathRewrite: {
+          '/abc': ''
+        }
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
